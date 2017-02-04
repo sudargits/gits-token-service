52739587d73226ccad85937af58785f61cde391e
GITS Indonesia <yatnosudar@gits.co.id>
2017-02-04 23:59:01 +0700

    mysql

--- Main.go
+++ Main.go
@@ -27,7 +27,7 @@ func main() {
 
 //SETUP DB
 var dba = func() (*gorm.DB){
-	dbs := DBToken{"mysql", "mysql", "21484eb879a418bb","token-service","172.17.0.2",nil}
+	dbs := DBToken{"mysql", "mysql", "21484eb879a418bb","token-service","tcp(172.17.0.2:3306)",nil}
 	dbs.getConnection()
 	return dbs.dbToken
 }
